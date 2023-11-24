6b08a8bb63cbc87b52a46050080c909bc563d6c2
FraBona <francescobonafede08@gmail.com>
2023-11-24 15:26:53 +0100

    add css

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
   <title>Document</title>
   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <link rel="stylesheet" href="./main.css">
 </head>
 <body>
 
   <div id="app">
-    <h1>{{message}}</h1>
+    <div class="container">
+      <h1>{{ message }}</h1>
+    </div>
   </div>
 
 
